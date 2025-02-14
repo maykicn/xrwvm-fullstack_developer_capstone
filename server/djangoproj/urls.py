9209da1821c758a11d88cde4d5f536281b82a8b4
maykicn <mayki.cn@gmail.com>
2025-02-14 06:06:41 -0500

    Add registeration

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,5 +1,4 @@
 """djangoproj URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -18,8 +17,10 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-
 urlpatterns = [
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('logout/', TemplateView.as_view(template_name="Home.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('admin/', admin.site.urls),
